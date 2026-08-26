3ee801ad18439a9a831827ad2bb6e27b5fcf6ff9
FredderickB <blancofredderick@gmail.com>
2026-08-26 02:26:23 -0400

    backend login complete, saves user to db and sends jwt to front

--- server/src/main/java/learn/blindchess/security/GoogleIdTokenVerifierImplementor.java
+++ server/src/main/java/learn/blindchess/security/GoogleIdTokenVerifierImplementor.java
@@ -5,9 +5,11 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 
+@Component
 public class GoogleIdTokenVerifierImplementor implements IdTokenVerifier{
 
     private final GoogleIdTokenVerifier verifier;
