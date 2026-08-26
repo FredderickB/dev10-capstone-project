c5f833663f719937998800cc5fafa7fc09079d8f
FredderickB <blancofredderick@gmail.com>
2026-08-26 03:49:41 -0400

    board rendering

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Chessboard } from 'react-chessboard';
+import { useState } from 'react';
 
 export default function Board() {
+
+  const [fen] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+
   return (
-    <div>Board</div>
-  )
+    <div style={{ width: '500px' }}>
+      <Chessboard
+        options={{
+          position: fen,
+          allowDragging: false,
+        }}
+      />
+    </div>
+  );
 }
