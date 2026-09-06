92c3cd5aabbfb7997aa23318b6e818d112a9593f
FredderickB <blancofredderick@gmail.com>
2026-09-05 23:46:39 -0400

    showing username on profile page

--- server/src/main/java/learn/blindchess/domain/UserService.java
+++ server/src/main/java/learn/blindchess/domain/UserService.java
@@ -1,9 +1,48 @@
 package learn.blindchess.domain;
 
 import learn.blindchess.data.DataAccessException;
+import learn.blindchess.data.UserRepository;
 import learn.blindchess.model.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface UserService {
+@Service
+public class UserServiceImplementor implements UserService {
 
-    Result<User> processGoogleUser(User user) throws DataAccessException;
+    private final UserRepository userRepository;
+
+    public UserServiceImplementor(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Result<User> findByEmail(String email) throws DataAccessException {
+
+        Result<User> result = new Result<>();
+        result.setPayload(userRepository.findByEmail(email));
+        return result;
+
+    }
+
+    @Transactional
+    public Result<User> processGoogleUser(User user) throws DataAccessException{
+
+        Result<User> result = new Result<>();
+
+        User existingUser = userRepository.findByEmail(user.getEmail());
+
+        if (existingUser != null) {
+            result.setPayload(existingUser);
+        } else {
+
+            User newUser = userRepository.create(user);
+
+            if (newUser == null || newUser.getUserId() == null) {
+                result.addErrorMessage("Failed to create user in database", ResultType.SERVER_ERROR);
+                return result;
+            }
+            result.setPayload(newUser);
+        }
+
+        return result;
+    }
 }
